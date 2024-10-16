55baf2a0ddd69426d165fa1c2c3cbe1c94ae9f84
elinfelicia <elinriihimaki@gmail.com>
2024-10-16 09:07:26 +0200

    quick styling

--- cosmic-escape/src/components/LandingPage.tsx
+++ cosmic-escape/src/components/LandingPage.tsx
@@ -5,12 +5,12 @@
        return (
            <div className="welcome-msg">
                <h1>Welcome to Cosmic Escape!</h1>
-               <p>
-                   🤖 "Greetings, human! You've woken up trapped on a spaceship about to implode! 
+               <h2>🤖 Greetings, human! </h2>
+               <p>You've woken up trapped on a spaceship about to implode! 
                    You have to escape before the timer runs out. Answer my questions correctly to 
                    move closer to the exit. But be careful, an incorrect answer will cost you time! 
-                   Now, are you ready to save your skin?" 
-               </p>
+                   Now, are you ready to save your skin? 
+                </p>
                <button onClick={onStart}>Start Game</button>
            </div>
        );
